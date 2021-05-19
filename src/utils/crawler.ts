0dbf823e2bf9268dc510b35a1d95825333997782
peek.lin <peek.lin@shijigroup.com>
2021-05-19 17:18:48 +0800

    改为百度翻译

--- src/utils/crawler.ts
+++ src/utils/crawler.ts
@@ -16,17 +16,47 @@ export default class Crawler {
 
   async typeWord(page: Page, word: string) {
     console.log(`translate: ${word}`)
-    const input = await page.$('textarea#source')
+    const input = await page.$('textarea#baidu_translate_input')
     input && input.type(word)
   }
 
+  // 谷歌翻译
+  // addListen(browser: Browser, page: Page) {
+  //   page.on('response', interceptedRes => {
+  //     const resUrl = interceptedRes.url()
+  //     if (/https:\/\/translate.google.cn\/translate_a\/single/.test(resUrl)) {
+  //       interceptedRes.text().then(async (data) => {
+  //         this.contentList[this.index] = JSON.parse(data)[0][0][0]
+  //         await page.click('.clear')
+  //         this.index = this.index + 2
+  //         if (this.index + 1 < this.contentList.length) {
+  //           setTimeout(() => {
+  //             this.typeWord(page, this.contentList[this.index])
+  //           }, 1000)
+  //         } else {
+  //           this.loading = false
+  //           this.resList = this.contentList
+  //           console.log('translated')
+  //           browser.close()
+  //         }
+  //       })
+  //     }
+  //   })
+  // }
+
+  // 百度翻译
   addListen(browser: Browser, page: Page) {
     page.on('response', interceptedRes => {
       const resUrl = interceptedRes.url()
-      if (/https:\/\/translate.google.cn\/translate_a\/single/.test(resUrl)) {
+      if (/https:\/\/fanyi.baidu.com\/v2transapi/.test(resUrl)) {
         interceptedRes.text().then(async (data) => {
-          this.contentList[this.index] = JSON.parse(data)[0][0][0]
-          await page.click('.clear')
+          this.contentList[this.index] = JSON.parse(data).trans_result.data[0].dst
+          await page.keyboard.type('');
+          await page.keyboard.down('Shift');
+          for (let i = 0; i < this.contentList[this.index].length; i++)
+          await page.keyboard.press('ArrowLeft');
+          await page.keyboard.up('Shift');
+          await page.keyboard.press('Backspace');
           this.index = this.index + 2
           if (this.index + 1 < this.contentList.length) {
             setTimeout(() => {
@@ -47,7 +77,9 @@ export default class Crawler {
     this.resList = []
     this.loading = true
     console.log('translating...')
-    const baseUrl = `https://translate.google.cn/#view=home&op=translate&sl=${this.from}&tl=${this.to}`
+    // const baseUrl = `https://translate.google.cn/#view=home&op=translate&sl=${this.from}&tl=${this.to}`
+    // const baseUrl = `https://translate.google.cn/?sl=${this.from}&tl=${this.to}&op=translate`
+    const baseUrl = `https://fanyi.baidu.com/?aldtype=16047#${this.from}/${this.to}/`
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     page.setDefaultNavigationTimeout(0)
